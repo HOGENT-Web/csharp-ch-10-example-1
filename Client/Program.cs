54e45de80fc9f493d5c77554624eba8137392676
Benjamin Vertonghen <benjamin.vertonghen@hogent.be>
2021-12-04 19:45:32 +0100

    Protect the Client

--- Client/Program.cs
+++ Client/Program.cs
@@ -7,5 +7,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+});
 
 await builder.Build().RunAsync();
